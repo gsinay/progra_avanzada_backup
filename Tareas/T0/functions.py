966537d1aab3f4d2d52cb84bce4108253093ec83
Gabriel <gsinayc@gmail.com>
2023-03-21 19:05:00 -0300

    terminar valor bombas

--- Tareas/T0/functions.py
+++ Tareas/T0/functions.py
@@ -7,10 +7,9 @@ def cargar_tablero(nombre_archivo: str) -> list:
     with open(os.path.join( "Archivos", nombre_archivo), "r") as lectura_de_archivo:
         lineas = lectura_de_archivo.readlines()
         dimension = int(lineas[0][0]) #dimension del tablero dado por el primer elemento
-        lineas = lineas[0][2:] #limpia la linea de data para excluir la informacion de la dimension del tablero
-        lista_general = lineas.split(",") #splita la string a una lista de dimension n^2x1
+        lista_general = lineas[0][2:].split(",") #data sin primer elemento dimension y en format lista
         lista_final = []
-        for i in range(0, len(lista_general), dimension): #cambia la lista a lista de listas cuadrada de dimensión del tablero
+        for i in range(0, len(lista_general), dimension): #cambia la lista a lista de listas cuadrada 
             posicion = i
             lista_final.append(lista_general[posicion : posicion + dimension])
         return lista_final
@@ -18,19 +17,22 @@ def cargar_tablero(nombre_archivo: str) -> list:
 
 def guardar_tablero(nombre_archivo: str, tablero: list) -> None:
     dimension = str(len(tablero))
-    data_a_guardar = str(dimension + ",") #empezamos armando la string a guardar con el formato especificado
+    data_a_guardar = str(dimension + ",") #empezamos armando la string con el formato especificado
     for fila in tablero:
         for columna in fila:
             data_a_guardar += str(columna) +"," #concatenamos la data de la cordenada (fila, columna) a la string
     data_a_guardar = data_a_guardar[:-1] #borramos el ultimo coma 
     
 
-
-    pass
-
-
 def verificar_valor_bombas(tablero: list) -> int:
-    pass
+    cantidad_bombas_invalidas = 0
+    for fila in tablero:
+        for elemento in fila:
+            if str(elemento).isnumeric(): #checkeando si es bomba
+                if int(elemento) < 2 or elemento > (2*len(tablero)) - 1:
+                    cantidad_bombas_invalidas += 1
+    return cantidad_bombas_invalidas
+
 
 
 def verificar_alcance_bomba(tablero: list, coordenada: tuple) -> int:
