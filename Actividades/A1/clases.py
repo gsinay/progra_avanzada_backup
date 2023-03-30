40282922001728959f25bfc4735219d0f44bd295
Gabriel <gsinayc@gmail.com>
2023-03-30 15:04:43 -0300

    respaldo 1

--- Actividades/A1/clases.py
+++ Actividades/A1/clases.py
@@ -1,17 +1,65 @@
-class Animal:
-    pass
+from abc import abstractmethod, ABC
+class Animal(ABC):
+    
+    def __init__(self, peso, nombre, *args, **kwargs):
+        super().__init__(*kwargs, **kwargs)
+        self.__energia = 100
+        
+    @abstractmethod
+    def desplazarse(self):
+        pass
+    
+    @property
+    def get_energia(self):
+        return self.__energia
+    
+    @get_energia
+    def set_energia(self, cambio_energia):
+        if self.__energia - cambio_energia < 0:
+            self.__energia = 0
+        else:
+            self.__energia -= cambio_energia
 
-class Terrestre:
-    pass
 
-class Acuatico:
-    pass
+class Terrestre(Animal, ABC):
+    def __init__(self, cantidad_patas, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cantidad_patas = cantidad_patas
+
+    def energia_gastada_por_desplazamiento(self) -> int:
+        return self.peso * 5
+    
+    def desplazarse(self) -> str:
+       self.set_energia(self.energia_gastada_por_desplazamiento())
+       return "caminando..."
+
+
+
+    
+class Acuatico(Animal, ABC):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def energia_gastada_por_desplazamiento(self) -> int:
+        return self.peso * 2
+    
+    def desplazarse(self) -> str:
+        self.set_energia(self.energia_gastada_por_desplazamiento())
+        return "nadando..."
+
 
 class Perro:
-    pass
+    def __init__(self, raza, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.raza = raza
+
+    def ladrar(self):
+        return "guau guau"
 
 class Pez:
-    pass
+    def __init__(self, color, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.color = color
 
 class Ornitorrinco:
     pass
