a9ca1a24a6203576e709ecb41fd4bbff1c12f542
Gabriel <gsinayc@gmail.com>
2023-06-22 15:08:38 -0400

    terminar API

--- Actividades/AB/yolanda.py
+++ Actividades/AB/yolanda.py
@@ -13,31 +13,72 @@ class Yolanda:
 
     def saludar(self) -> dict:
         # TODO: Completar
-        return "Completar"
+        respuesta = requests.get(f"{self.base}/")
+        return {"status-code": respuesta.status_code, "saludo": respuesta.json()["result"]}
+
 
     def verificar_horoscopo(self, signo: str) -> bool:
         # TODO: Completar
+        endpoint = "/signos"
+        respuesta = requests.get(self.base + endpoint)
+        return signo in respuesta.json()["result"]
         return "Completar"
 
     def dar_horoscopo(self, signo: str) -> dict:
         # TODO: Completar
-        return "Completar"
+        parametros = {"signo": signo}
+        endpoint = "/horoscopo"
+        respusta = requests.get(self.base + endpoint, params=parametros)
+        return {"status-code": respusta.status_code, "mensaje": respusta.json()["result"]}
 
     def dar_horoscopo_aleatorio(self) -> dict:
         # TODO: Completar
-        return "Completar"
+        endpoint = "/aleatorio"
+        respuesta = requests.get(self.base + endpoint)
+        if respuesta.status_code != 200:
+            return {"status-code": respuesta.status_code, "mensaje": respuesta.json()["result"]}
+        else:
+            enlace = respuesta.json()["result"]
+            respuesta_nueva = requests.get(enlace)
+            return {"status-code": respuesta_nueva.status_code, "mensaje": respuesta_nueva.json()["result"]}
 
     def agregar_horoscopo(self, signo: str, mensaje: str, access_token: str) -> str:
         # TODO: Completar
-        return "Completar"
+        my_header = {"authorization": access_token}
+        endpoint = "/update"
+        respuesta = requests.post(self.base + endpoint, headers=my_header, data = {"signo": signo, "mensaje": mensaje})
+        if respuesta.status_code == 401:
+            return "Agregar horoscopo no autorizado"
+        elif respuesta.status_code == 400:
+            return respuesta.json()["result"]
+        else:
+            return "La base de YolandaAPI ha sido actualizada"
+
 
     def actualizar_horoscopo(self, signo: str, mensaje: str, access_token: str) -> str:
         # TODO: Completar
-        return "Completar"
+        endpoint = "/update"
+        my_header = {"authorization": access_token}
+        respuesta = requests.put(self.base + endpoint, headers=my_header, data = {"signo": signo, "mensaje": mensaje})
+        if respuesta.status_code == 401:
+            return "Editar horoscopo no autorizado"
+        elif respuesta.status_code == 400:
+            return respuesta.json()["result"]
+        else:
+            return "La base de YolandaAPI ha sido actualizada"
+
 
     def eliminar_signo(self, signo: str, access_token: str) -> str:
         # TODO: Completar
-        return "Completar"
+        endpoint = "/remove"
+        my_header = {"authorization": access_token}
+        respuesta = requests.delete(self.base + endpoint, headers=my_header, data = {"signo": signo})
+        if respuesta.status_code == 401:
+            return "Eliminar signo no autorizado"
+        elif respuesta.status_code == 400:
+            return respuesta.json()["result"]
+        else:
+            return "La base de YolandaAPI ha sido actualizada"
 
 
 if __name__ == "__main__":
